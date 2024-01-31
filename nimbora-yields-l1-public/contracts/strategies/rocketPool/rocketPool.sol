792bc0910953261215a143bb2ebb2ccfde341e07
Daniel Bejarano <dbejarano820@gmail.com>
2024-01-30 22:49:00 -0600

    rocket pool strategy init

---
+++ nimbora-yields-l1-public/contracts/strategies/rocketPool/rocketPool.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.20;
+
+import {StrategyBase} from "../StrategyBase.sol";
+import {ErrorLib} from "../../lib/ErrorLib.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ISavingDai} from "../../interfaces/ISavingDai.sol";
+import {IRETH} from "../../interfaces/IRETH.sol";
+import {IRocketDepositPool} from "../../interfaces/IRocketDepositPool.sol";
+import {IRocketStorage} from "../../interfaces/IRocketStorage.sol";
+
+
+contract RocketPoolStakingStrategy is StrategyBase {
+     IRocketStorage public rocketStorage;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {}
+
+    function initialize(
+        address _poolingManager,
+        address _underlyingToken,
+        address _yieldToken,
+        address _rocketStorageAddress
+    ) public virtual initializer {
+        //underlying token hardcoded? RP dice generar dinamicamente
+        initializeStrategyBase(_poolingManager, _underlyingToken, _yieldToken);
+        _initializeRocketPool(_rocketStorageAddress);
+    }
+
+    function _initializeRocketPool(address _rocketStorageAddress) internal {
+        require(_rocketStorageAddress != address(0), "Zero address: RocketPool");
+        rocketStorage = IRocketStorage(_rocketStorageAddress);
+    }
+
+    function _getRocketDepositPool() internal view returns (IRocketDepositPool) {
+        address rocketDepositPoolAddress = rocketStorage.getAddress(keccak256(abi.encodePacked("contract.address", "rocketDepositPool")));
+        return IRocketDepositPool(rocketDepositPoolAddress);
+    }
+
+    function _deposit(uint256 amount) internal override {
+        IRocketDepositPool rocketDepositPool = _getRocketDepositPool();
+        rocketDepositPool.deposit{value: amount}();
+    }
+
+    function _withdraw(uint256 amount) internal override returns (uint256) {
+         address rocketTokenRETHAddress = rocketStorage.getAddress(keccak256(abi.encodePacked("contract.address", "rocketTokenRETH")));
+         IRETH rocketTokenRETH = IRETH(rocketTokenRETHAddress);
+         require(rocketTokenRETH.transfer(address(this), amount), "rETH was not transferred to caller");
+         return (amount);
+    }
+
+    //?
+    function _underlyingToYield(uint256 amount) internal view override returns (uint256) {
+        return ISavingDai(yieldToken).previewDeposit(amount);
+    }
+
+    //?
+    function _yieldToUnderlying(uint256 amount) internal view override returns (uint256) {
+        return ISavingDai(yieldToken).previewRedeem(amount);
+    }
+}
